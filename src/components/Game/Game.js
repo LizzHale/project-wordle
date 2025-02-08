f15e9f933ba0fd56ad32a9a677797c3d7860cc88
Lizz Hale <6598185+LizzHale@users.noreply.github.com>
2025-02-08 15:25:55 +0000

    Text input is controlled by React state. Log guess to console. When form is submitted, check the guess against the answer. Log result to console

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
+import { checkGuess } from '../../game-helpers';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -9,7 +10,22 @@ const answer = sample(WORDS);
 console.info({ answer });
 
 function Game() {
-  return <>Put a game here!</>;
+  const [guess, setGuess] = useState("")
+  console.info({guess})
+
+  return (
+    <form
+      className="guess-input-wrapper"
+      onSubmit={(event) => {
+        event.preventDefault();
+        result = checkGuess(guess, answer);
+        console.log(result)
+      }}
+    >
+      <label for="guess-input">Enter guess:</label>
+      <input id="guess-input" type="text" value={guess} onChange={(event) => {setGuess(event.target.value)}}/>
+    </form>
+  )
 }
 
 export default Game;
