cea3170d1b58a177d46f578616dd111af2eeb33a
Lizz Hale <6598185+LizzHale@users.noreply.github.com>
2025-02-11 04:44:20 +0000

    Use unique keys for each list item

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { range } from '../../utils';
 
 function GuessResults({ guessList }) {
+  const rowKey = ({ rowIndex }) => {
+    console.log(`empty-guess-row${rowIndex}`)
+    return `empty-guess-row${rowIndex}`
+  };
+
   return (
     <div className="guess-results">
       {range(0,6).map((y) => (
-        <p className="guess">
+        <p key={guessList[y] ? guessList[y].id : `empty-guess-row${y}`} className="guess">
           {range(0,5).map((x) => (
-            <span className="cell">{guessList[y] && guessList[y].result[x] && guessList[y].result[x].letter}</span>
+            <span key={`empty-guess-column${x}`} className="cell">{guessList[y] && guessList[y].result[x] && guessList[y].result[x].letter}</span>
           ))}
         </p>
       ))}
