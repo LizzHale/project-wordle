c9550b85c439dbd7c3d9db2c7affbde2888e3523
Lizz Hale <6598185+LizzHale@users.noreply.github.com>
2025-02-15 18:24:12 +0000

    Use NUM_OF_GUESSES_ALLOWED const to control game board and guesses

--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { range } from '../../utils';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 function GuessResults({ guessList }) {
   return (
     <div className="guess-results">
-      {range(0,6).map((y) => (
+      {range(0,NUM_OF_GUESSES_ALLOWED).map((y) => (
         <p key={guessList[y] ? guessList[y].id : `empty-guess-row${y}`} className="guess">
           {range(0,5).map((x) => (
             <span key={`empty-guess-column${x}`} className="cell">{guessList[y] && guessList[y].result[x] && guessList[y].result[x].letter}</span>
